91e116e5dbb035947c9ef1ad961057d17f58d027
jh27kim <jh27kim@gmail.com>
2021-03-31 22:19:03 +0900

    git

---
+++ TerrainFormatting.py
@@ -0,0 +1,44 @@
+from collections import defaultdict
+
+
+def solution(land, P, Q):
+    answer = -1
+    N = len(land)
+    blocks = defaultdict(int)
+
+    for i in range(N):
+        for j in range(N):
+            blocks[land[i][j]] += 1
+
+    def cal(k):
+        p, q = 0, 0
+        for key, value in blocks.items():
+            if key < k:
+                p += (k - key) * value
+            elif key > k:
+                q += (key - k) * value
+
+        return p * P + q * Q
+
+    bottom = min(blocks)
+    top = max(blocks)
+    while True:
+        mid = (bottom + top) // 2
+
+        upperBound = cal(mid + 1)
+        cur = cal(mid)
+        lowerBound = cal(mid - 1)
+
+        if cur <= lowerBound and cur <= upperBound:
+            answer = cur
+            break
+
+        elif lowerBound < cur:
+            top = mid - 1
+
+        elif upperBound < cur:
+            bottom = mid + 1
+
+    return answer
+
+
---
+++ WordPuzzle.py
@@ -0,0 +1,20 @@
+def solution(strs, t):
+    answer = 0
+    MAX = int(1e9)
+    n = len(t)
+    dp = [MAX] * (n + 1)
+    dp[0] = 0
+    strs = set(strs)
+
+    for i in range(1, n + 1):
+        for k in range(1, 6):
+            if i - k < 0:
+                s = 0
+            else:
+                s = i - k
+            if t[s:i] in strs:
+                dp[i] = min(dp[i], dp[i - k] + 1)
+
+    print(dp)
+
+    return dp[-1] if dp[-1] != MAX else -1
